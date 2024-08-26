fc9a7c06aa3d076f5f5ad36ddd2007651a1c457d
Viraj Nalbalwar <virajnalbalwar@gmail.com>
2024-08-26 17:51:54 +0530

    #

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import string
-from flask import json
 from flask import Flask, request, jsonify, render_template, send_from_directory, send_file
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
-from dotenv import find_dotenv, load_dotenv
+from transformers import BlipProcessor, BlipForConditionalGeneration, pipeline
+import torch
 from gtts import gTTS
-from transformers import pipeline
 import language_tool_python
-
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -18,15 +16,8 @@ app.config['AUDIO_FOLDER'] = 'audio'
 os.makedirs(app.config['AUDIO_FOLDER'], exist_ok=True)
 
 def correct_grammar(text):
-    # Initialize the language tool
     tool = language_tool_python.LanguageTool('en-US')
-
-    # Perform grammar check
-    matches = tool.check(text)
-
-    # Apply corrections
     corrected_text = tool.correct(text)
-
     return corrected_text
 
 def image2text(image_path, conditional_keyword):
@@ -36,10 +27,9 @@ def image2text(image_path, conditional_keyword):
     raw_image = Image.open(image_path).convert('RGB')
 
     # Conditional image captioning
-    text = f"{conditional_keyword}" if conditional_keyword else "The"
+    text = conditional_keyword if conditional_keyword else "The"
     inputs = processor(raw_image, text, return_tensors="pt")
     min_tokens = 30
-    # max_new_tokens = 3000
     out = model.generate(**inputs, min_length=min_tokens, max_length=min_tokens+5)
     conditional_description = processor.decode(out[0], skip_special_tokens=True)
 
@@ -48,13 +38,11 @@ def image2text(image_path, conditional_keyword):
     out = model.generate(**inputs, min_length=min_tokens, max_length=min_tokens+5)
     unconditional_description = processor.decode(out[0], skip_special_tokens=True)
 
-    # return conditional_description.capitalize(), unconditional_description.capitalize()
     # Apply grammar correction
     corrected_conditional_description = correct_grammar(conditional_description.capitalize())
     corrected_unconditional_description = correct_grammar(unconditional_description.capitalize())
 
     return corrected_conditional_description, corrected_unconditional_description
-
 
 def text2speech(text, filename):
     audio_path = os.path.join(app.config['AUDIO_FOLDER'], filename)
@@ -63,55 +51,37 @@ def text2speech(text, filename):
     return audio_path
 
 def sentiment(text):
-    pipe = pipeline("text-classification", model="SamLowe/roberta-base-go_emotions")
+    device = 0 if torch.cuda.is_available() else -1
+    pipe = pipeline("text-classification", model="SamLowe/roberta-base-go_emotions", device=device)
     result = pipe(text)
-    
-    # Extracting emotion labels and the corresponding scores, converting scores to percentages
     emotions_list = [{"label": entry['label'], "score": round(entry['score'] * 100, 3)} for entry in result]
-
     return emotions_list
 
 def automatic_speech_recognition_function(audio_path):
-    # Load the ASR pipeline
-    cls = pipeline("automatic-speech-recognition", model="distil-whisper/distil-large-v3")
-    # Perform ASR on the audio file
-    result = cls(audio_path)
-    # Extract the transcribed text
+    device = 0 if torch.cuda.is_available() else -1
+    asr_pipeline = pipeline("automatic-speech-recognition", model="distil-whisper/distil-large-v3", device=device)
+    result = asr_pipeline(audio_path)
     transcribed_text = result['text']
     return transcribed_text
 
-def text_summarization(text):
-    summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
-
-    summary = summarizer(text, max_length=130, min_length=30, do_sample=False) 
-
-    result = summary[0]['summary_text']
-    return result
-
-
-
-
-
-############################################################################################
-
+##################################################
 
 @app.route('/', methods=['GET', 'POST'])
-def startPoint():
+def start_point():
     return render_template('bootstrap_website.html')
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Check if 'image' and 'conditional_keyword' are in request.files and request.form respectively
         if 'image' not in request.files or 'conditional_keyword' not in request.form:
-            return jsonify({"conditional_audio_path": None, "unconditional_audio_path": None, 
+            return jsonify({"conditional_audio_path": None, "unconditional_audio_path": None,
                             "conditional_description": "No image or keyword uploaded", "unconditional_description": "No image uploaded"})
 
         image = request.files['image']
         conditional_keyword = request.form['conditional_keyword']
 
         if image.filename == '':
-            return jsonify({"conditional_audio_path": None, "unconditional_audio_path": None, 
+            return jsonify({"conditional_audio_path": None, "unconditional_audio_path": None,
                             "conditional_description": "No selected image", "unconditional_description": "No selected image"})
 
         if image:
@@ -119,7 +89,6 @@ def index():
             image.save(image_path)
             conditional_description, unconditional_description = image2text(image_path, conditional_keyword)
 
-            # Convert text to audio for both conditional and unconditional descriptions
             conditional_audio_path = text2speech(conditional_description, 'conditional_audio.mp3')
             unconditional_audio_path = text2speech(unconditional_description, 'unconditional_audio.mp3')
 
@@ -145,7 +114,6 @@ def sentiment_analysis():
         formatted_sentiment_result = [f"{entry['label'].capitalize()} : {entry['score']:.3f}" for entry in sentiment_result]
         result_string = ", ".join(formatted_sentiment_result)
     
-        # return jsonify({"sentiment": sentiment_result}) #To get this kind of output --> [{"label":"joy","score":89.194}]
         return jsonify({"sentiment": result_string})
     return render_template('sentiment.html')
 
@@ -161,14 +129,11 @@ def automatic_speech_recognition():
             return jsonify({"transcribed_text": "No selected audio file"})
 
         if audio_file:
-            # Save the uploaded audio file
             audio_path = os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded_audio.mp3')
             audio_file.save(audio_path)
 
-            # Perform automatic speech recognition
             transcribed_text = automatic_speech_recognition_function(audio_path)
 
-            # Remove punctuation from the transcribed text
             transcribed_text_cleaned = ''.join(char for char in transcribed_text if char not in string.punctuation)
 
             response_data = {
@@ -177,11 +142,9 @@ def automatic_speech_recognition():
             }
 
             return jsonify(response_data)
-            # return transcribed_text_cleaned
 
         return jsonify({"transcribed_text": "Unexpected error during audio processing"})
 
-    # Render the form for GET requests
     return render_template('automatic_speech_recognition.html')
 
 @app.route('/audio/<filename>')
@@ -193,12 +156,10 @@ def text_to_audio():
     if request.method == 'POST':
         text = request.form['text']
         tts = gTTS(text)
-        tts.save('output.mp3')
-        return send_file('output.mp3', as_attachment=True)
+        output_path = os.path.join(app.config['AUDIO_FOLDER'], 'output.mp3')
+        tts.save(output_path)
+        return send_file(output_path, as_attachment=True)
     return render_template('text_to_audio.html')
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
